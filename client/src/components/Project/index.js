ee18f042f044706131f009ef92e1d41342ee9d28
coyote <megan.metelak@gmail.com>
2022-04-20 17:24:29 -0500

    moved over project component file with edits

---
+++ client/src/components/Project/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
+
+function Project({ project }) {
+  const { name, repo, link, description } = project;
+
+  return (
+    <div className="project" key={name}>
+      <img
+        src={require(`../../assets/projects/${name}.jpg`)}
+        alt={capitalizeFirstLetter(name)}
+        className="project-bg"
+      />
+      <div className="project-text">
+        <h3>
+          <a href={link}>{capitalizeFirstLetter(name)}</a>{" "}
+          <a href={repo}>
+            <i className="fab fa-github"></i>
+          </a>
+        </h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
