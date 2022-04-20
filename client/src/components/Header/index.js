1e62affcf303b731b6d30531a149a7ebb59bb42e
coyote <megan.metelak@gmail.com>
2022-04-20 17:29:55 -0500

    moving over header component

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import coverImage from '../../assets/cover/cover-image.jpg'
+
+function Header(props) {
+
+    return (
+      <div>
+        <header>
+          <h1>M.Metelak</h1>
+          <img src={coverImage} alt="something"></img>
+          {props.children}
+        </header>
+      </div>
+    );
+  }
+  
+export default Header;
