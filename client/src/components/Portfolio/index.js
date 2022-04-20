ec6619daa2accecb0c86b7be5f11231a626bbc98
coyote <megan.metelak@gmail.com>
2022-04-20 17:26:13 -0500

    moved over portfolio component file with edits

---
+++ client/src/components/Portfolio/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Project from "../Project";
+
+function Portfolio() {
+  const [projects] = useState([
+    {
+      name: "weathered-vibes",
+      description: "frontend project",
+      link: "https://sepidehayani.github.io/WeatheredVibes/",
+      repo: "https://github.com/SepidehAyani/WeatheredVibes",
+    },
+    {
+      name: "travel-bug",
+      description: "full-stack project",
+      link: "https://travelbug-project.herokuapp.com/",
+      repo: "https://github.com/Metelak/Travel-bug",
+    },
+    {
+      name: "README-generator",
+      description: "Node",
+      link: "https://github.com/Metelak/README-generator",
+      repo: "https://github.com/Metelak/README-generator",
+    },
+  ]);
+
+  return (
+    <div>
+      <div className="flex-row">
+        {projects.map((project, idx) => (
+          <Project project={project} key={"project" + idx} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
