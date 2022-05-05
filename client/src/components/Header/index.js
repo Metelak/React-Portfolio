0d012b63724fe8eda2ccb531c729cb6a4859460b
coyote <megan.metelak@gmail.com>
2022-05-05 16:22:21 -0500

    resized profile picture and changed some colors

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import coverImage from '../../assets/cover/cover-image.jpg';
+// import coverImage from '../../assets/cover/cover-image.jpg';
 
 function Header(props) {
 
   return (
     <header className="flex-row space-between px-1">
       <h1>Metelak</h1>
-      <img src={coverImage} alt="background"></img>
+      {/* <img src={coverImage} alt="background"></img> */}
       {props.children}
     </header>
   );
